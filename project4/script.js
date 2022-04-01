82f7a1ee01b9fbf1168b8fea7409031dfb22c517
mishti169 <manishapanchal169@gmail.com>
2022-04-01 12:42:09 +0530

    update   lower count and add count with js

--- project4/script.js
+++ project4/script.js
@@ -1,1 +1,55 @@
-console.log('hii');
+console.log("hii I'm Mishti here");
+
+// approach 1
+/* const valueTag = document.querySelector('#value');
+const addCount = document.querySelector('#addCount');
+const lowerCount = document.querySelector('#lowerCount');
+
+addCount.addEventListener('click', callAddCounterValue);
+lowerCount.addEventListener('click', callLowerCounterValue);
+
+function getCurrentValue() {
+  return Number(valueTag.innerText);
+}
+
+function callAddCounterValue() {
+  let currentVal = getCurrentValue();
+  valueTag.innerText = currentVal + 1;
+}
+
+function callLowerCounterValue() {
+  let currentVal = getCurrentValue();
+  valueTag.innerText = currentVal - 1;
+}
+ */
+
+// approach 2
+
+// add count btn click should increment count
+const btnAdd = document.querySelector('#addCount');
+const btnMinus = document.querySelector('#lowerCount');
+
+btnAdd.addEventListener('click', handleCount);
+btnMinus.addEventListener('click', handleCount);
+
+function handleCount(event) {
+  const clickedBtnId = event.target.id;
+  const valueTag = document.querySelector('#value');
+  const currentValue = Number(valueTag.innerText);
+  let ans;
+  if (clickedBtnId === 'addCount') {
+    ans = currentValue + 1;
+  } else {
+    ans = currentValue - 1;
+  }
+
+  valueTag.innerText = ans;
+
+  if (ans > 0) {
+    valueTag.style.color = 'green';
+  } else if (ans < 0) {
+    valueTag.style.color = 'red';
+  } else {
+    valueTag.style.color = 'black';
+  }
+}
